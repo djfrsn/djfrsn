bedd252cd54d264f2adf5cafdd6389271732a765
djfrsn <dj@dennisjefferson.com>
2015-12-12 22:21:15 -0800

    Update for React

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: 'eval-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, './public/js/main.js')
+    path.join(__dirname, './public/js/shell.js')
   ],
   output: {
     path: path.join(__dirname, '/dist'),
