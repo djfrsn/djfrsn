3fb0615c83c7439f76d166c2b3fa1f0cb2e2630e
Dennis Jefferson <dennismjeferson@gmail.com>
2022-06-22 23:56:10 -0700

    update path

--- pages/api/context.ts
+++ pages/api/context.ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import { MicroRequest } from 'apollo-server-micro/dist/types';
 import { ServerResponse } from 'http';
-import prisma from 'lib/prisma';
+
+import prisma from '../../lib/prisma';
 
 export interface Context {
   req: MicroRequest
