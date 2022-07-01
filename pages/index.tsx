3ba2cf16bb67f0fff06e19053917b0617aac6cfd
Dennis Jefferson <dennismjeferson@gmail.com>
2022-07-01 16:02:15 -0700

    Set redirect in next config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,4 @@
-import { createClient } from 'prismicio';
-
-export async function getStaticProps() {
-  const client = createClient()
-  const [page, global] = await Promise.all([
-    client.getSingle('homepage'),
-    client.getSingle('global'),
-  ])
-
-  console.log('PATH', {
-    destination: `/${page.data.redirectPath.uid}`,
-    permanent: false,
-  })
-
-  return {
-    props: {
-      page,
-      global,
-      redirect: {
-        destination: `/${page.data.redirectPath.uid}`,
-        permanent: false,
-      },
-    },
-  }
-}
-
-const Homepage = ({ page, global }) => {
+const Homepage = () => {
   return null
 }
 
