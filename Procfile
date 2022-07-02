d310e768843096ca4a8373e4ddc41ac7506748c6
Dennis Jefferson <dennismjeferson@gmail.com>
2022-07-02 14:36:12 -0700

    Use throng to handle worker restarting

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: node server.js
-worker: node worker.js
+web: npm start
+worker: npm run workers
--- lib/workers/index.ts
+++ lib/workers/index.ts
@@ -1,45 +1,49 @@
 import { QUEUE } from 'lib/const';
 import gracefulShutdown from 'lib/utils/gracefulShutdown';
+import throng from 'throng';
 
 import marketIndexCronProcessor from './marketIndex/cron';
 import refreshMarketIndexProcessor from './marketIndex/refresh';
 import refreshMarketIndexTickerProcessor from './marketIndex/refreshTicker';
 import { createWorker } from './worker.factory';
 
-const {
-  worker: refreshMarketIndexCronWorker,
-  scheduler: refreshMarketIndexCronWorkerScheduler,
-} = createWorker(QUEUE.refresh.marketIndexes, marketIndexCronProcessor)
+const start = () => {
+  const {
+    worker: refreshMarketIndexCronWorker,
+    scheduler: refreshMarketIndexCronWorkerScheduler,
+  } = createWorker(QUEUE.refresh.marketIndexes, marketIndexCronProcessor)
 
-const {
-  worker: refreshMarketIndexWorker,
-  scheduler: refreshMarketIndexWorkerScheduler,
-} = createWorker(QUEUE.refresh.marketIndex, refreshMarketIndexProcessor)
+  const {
+    worker: refreshMarketIndexWorker,
+    scheduler: refreshMarketIndexWorkerScheduler,
+  } = createWorker(QUEUE.refresh.marketIndex, refreshMarketIndexProcessor)
 
-const {
-  worker: refreshMarketIndexTickerWorker,
-  scheduler: refreshMarketIndexTickerWorkerScheduler,
-} = createWorker(
-  QUEUE.refresh.marketIndexTicker,
-  refreshMarketIndexTickerProcessor,
-  5
-)
+  const {
+    worker: refreshMarketIndexTickerWorker,
+    scheduler: refreshMarketIndexTickerWorkerScheduler,
+  } = createWorker(
+    QUEUE.refresh.marketIndexTicker,
+    refreshMarketIndexTickerProcessor,
+    5
+  )
+  const onShutdown = async () => {
+    console.info('SIGTERM signal received: closing queues')
 
-const onShutdown = async () => {
-  console.info('SIGTERM signal received: closing queues')
+    await refreshMarketIndexCronWorker.close()
+    await refreshMarketIndexCronWorkerScheduler.close()
+    await refreshMarketIndexWorker.close()
+    await refreshMarketIndexWorkerScheduler.close()
+    await refreshMarketIndexTickerWorker.close()
+    await refreshMarketIndexTickerWorkerScheduler.close()
 
-  await refreshMarketIndexCronWorker.close()
-  await refreshMarketIndexCronWorkerScheduler.close()
-  await refreshMarketIndexWorker.close()
-  await refreshMarketIndexWorkerScheduler.close()
-  await refreshMarketIndexTickerWorker.close()
-  await refreshMarketIndexTickerWorkerScheduler.close()
+    console.info('All closed')
+  }
 
-  console.info('All closed')
+  process.on('SIGTERM', onShutdown)
+
+  gracefulShutdown(() => {
+    process.removeAllListeners('SIGTERM')
+  })
 }
 
-process.on('SIGTERM', onShutdown)
-
-gracefulShutdown(() => {
-  process.removeAllListeners('SIGTERM')
-})
+throng({ worker: start, count: 1 })
