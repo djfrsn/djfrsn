28a7947ebf31f2f190fb4035b73c37ebfab9e884
Dennis Jefferson <dennismjeferson@gmail.com>
2022-07-03 11:08:37 -0700

    Correct stylesheet location

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,12 @@ if (process.env.NEXT_MANUAL_SIG_HANDLE) {
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
