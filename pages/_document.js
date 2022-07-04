f9c68a677d44136180bdfc80931a7d9ed7137dd2
Dennis Jefferson <dennismjeferson@gmail.com>
2022-07-03 19:30:27 -0700

    Restore shutdown method

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,19 @@
+import gracefulShutdown from 'lib/utils/gracefulShutdown';
 import { Head, Html, Main, NextScript } from 'next/document';
+
+if (process.env.NEXT_MANUAL_SIG_HANDLE) {
+  process.on('SIGTERM', async () => {
+    console.log('Received SIGTERM: ', 'cleaning up')
+    gracefulShutdown()
+    process.exit(0)
+  })
+
+  process.on('SIGINT', async () => {
+    console.log('Received SIGINT: ', 'cleaning up')
+    gracefulShutdown()
+    process.exit(0)
+  })
+}
 
 export default function Document() {
   return (
