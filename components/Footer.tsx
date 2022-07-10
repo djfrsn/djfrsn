cd8c69d192bfb3163ea88a70a31961cfdfcb9eec
Dennis Jefferson <dennismjeferson@gmail.com>
2022-07-09 20:59:49 -0700

    Fix footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,30 +4,36 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { MdOutlineAlternateEmail } from 'react-icons/md';
 
-function FooterLinkItem({ data }) {
+function FooterLinkItem({ data, ...props }) {
   switch (true) {
     case typeof data.linkImage?.url === 'string':
       return (
-        <a className="link flex w-[24px]" target="_blank">
-          <Image
-            src={data.linkImage.url}
-            alt={data.linkImage.alt || data.linkTitle}
-            height={data.linkImage.dimensions.height}
-            width={data.linkImage.dimensions.width}
-          />
-        </a>
+        <Link href={parseUrl(data.link.url)}>
+          <a className="link flex w-[24px]" target="_blank">
+            <Image
+              src={data.linkImage.url}
+              alt={data.linkImage.alt || data.linkTitle}
+              height={data.linkImage.dimensions.height}
+              width={data.linkImage.dimensions.width}
+            />
+          </a>
+        </Link>
       )
     case data.link.url.includes('mailto:'):
       return (
-        <a className="link flex" target="_blank">
-          <MdOutlineAlternateEmail />
-        </a>
+        <Link href={parseUrl(data.link.url)}>
+          <a className="link" target="_blank">
+            <MdOutlineAlternateEmail />
+          </a>
+        </Link>
       )
     default:
       return (
-        <a className="link" target="_blank">
-          {data.linkTitle}
-        </a>
+        <Link href={parseUrl(data.link.url)}>
+          <a className="link no-underline" target="_blank">
+            {data.linkTitle}
+          </a>
+        </Link>
       )
   }
 }
@@ -40,9 +46,7 @@ function Footer({ data }: { data: FooterType }) {
           {data.links.map((footerLink, i) => {
             return (
               <li key={i} className="list-style-custom items-center">
-                <Link href={parseUrl(footerLink.link.url)}>
-                  <FooterLinkItem data={footerLink} />
-                </Link>
+                <FooterLinkItem data={footerLink} />
               </li>
             )
           })}
