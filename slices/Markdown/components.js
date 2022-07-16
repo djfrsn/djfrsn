4cd6bc2c47e4c9b1b3abe0e15afd8612b62b7df7
Dennis Jefferson <dennismjeferson@gmail.com>
2022-07-16 12:43:54 -0700

    Use Link for internal links

--- slices/Markdown/components.js
+++ slices/Markdown/components.js
@@ -1,17 +1,18 @@
-import isAbsoluteUrl from 'is-absolute-url';
+import parseUrl from 'lib/utils/parseUrl';
 import Link from 'next/link';
 
 // Docs: https://github.com/remarkjs/react-markdown#use-custom-components-syntax-highlight
 
 const markdownComponents = {
-  a: ({ node, ...props }) =>
-    isAbsoluteUrl(props.href) ? (
-      <a className="link" {...props} target="_blank" />
-    ) : (
-      <Link {...props}>
-        <a {...props}></a>
+  a: ({ node, href, ...props }) => {
+    return href.includes(process.env.NEXT_PUBLIC_SITE_URL) ? (
+      <Link href={parseUrl(href)} {...props}>
+        <a className="link" {...props}></a>
       </Link>
-    ),
+    ) : (
+      <a className="link" href={href} {...props} target="_blank" />
+    )
+  },
   ul: ({ node, children, ordered, ...props }) => (
     <ul
       className="list-style-disc pl-4"
